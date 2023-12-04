61e5950c175c95e8694a14188dfeed84d0cda03b
Jesse Josiah <jessezephyr@gmail.com>
2023-12-04 16:54:48 +0100

    update on verify company

--- models/VerifyCompany.js
+++ models/VerifyCompany.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 
 const CompanySchema = new mongoose.Schema({
     companyName: {type: String, required: true},
-    companyHQ: {type: String, required: true},
+    companyHq: {type: String, required: true},
     website: {type: String, required: true},
     companyEmail: {type: String, required: true},
     memberContact: {type: String, required: true},
     isVerified: {type: Boolean, default: false},
-    userId: {type: mongoose.Schema.Types.ObjectId, required: true, unique: true
-    }
+    // userId: {type: mongoose.Schema.Types.ObjectId, required: true, unique: true
+    // }
 }, {timestamps: true});
 
 module.exports = mongoose.model('VerifyCompany', CompanySchema)
