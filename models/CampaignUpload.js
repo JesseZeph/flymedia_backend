bb2527bfb09c67bf2e19e3e1a4eae581baa79ba0
Jesse Josiah <jessezephyr@gmail.com>
2023-12-06 15:20:29 +0100

    campaign

--- models/CampaignUpload.js
+++ models/CampaignUpload.js
@@ -18,7 +18,7 @@ const companyDetailsSchema = new mongoose.Schema({
   country: { type: String, required: true },
   rateFrom: { type: String, required: true },
   rateTo: { type: String, required: true },
-  viewsRequired: { type: String, required: true, enum: ['10,000 - 50,000', '50,000 - 200,000', '200,000 - 500,000', '500,000 - 1,000,000', '1,000,000 - 3,000,000'] },
+  viewsRequired: { type: String, required: true, enum: ['10k - 50k', '50k - 200k', '200k - 500k', '500k - 1m', '1m and above'] },
   jobDescription: { type: String, required: true },
 });
 
