5a5947137b039b2c083d779d71e66819c94086e9
Jesse Josiah <jessezephyr@gmail.com>
2023-12-06 01:45:41 +0100

    campaign

--- routes/campaingnUpload.js
+++ routes/campaingnUpload.js
@@ -4,7 +4,7 @@ const {verifyAndAuthorization } = require('../middleware/verifyToken')
 const upload = require('../middleware/multer');
 
 router.post('/', verifyAndAuthorization, upload.single('image'),UploadImageController.uploadCampaignImageAndDesc )
-router.get('/:id', verifyAndAuthorization, UploadImageController.getCampaignImageAndDesc )
+router.get('/:id', UploadImageController.getCampaignImageAndDesc )
 router.get('/', UploadImageController.getAllCampaignImageAndDesc )
 
 module.exports = router;
