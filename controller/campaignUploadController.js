167268f77a824250c53d5b38424b52e203afc21a
Shalom Abrokwah <shalomabrokwah@gmail.com>
2023-12-07 13:52:03 +0100

    new pull

--- controller/campaignUploadController.js
+++ controller/campaignUploadController.js
@@ -1,104 +1,126 @@
 const cloudinary = require('../utils/cloudinary');
 const CampaignUpload = require('../models/CampaignUpload');
-const Company = require('../models/VerifyCompany')
-const User = require('../models/User')
-
+const Company = require('../models/VerifyCompany');
+const User = require('../models/User');
 
 module.exports = {
-    uploadCampaignImageAndDesc: async (req, res) => {
-        try {
-            const userId = req.user.id;
-            const company = await Company.findOne({ userId: userId });
+  uploadCampaignImageAndDesc: async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const company = await Company.findOne({ userId: userId });
 
-            if (!company) {
-                return res.status(404).json({ success: false, message: "Company not found for the user" });
-            }
+      if (!company) {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Company not found for the user' });
+      }
 
-            if (!company.isVerified) {
-                return res.status(403).json({ success: false, message: "Company not verified. Please wait for admin approval." });
-            }
+      if (!company.isVerified) {
+        return res.status(403).json({
+          success: false,
+          message: 'Company not verified. Please wait for admin approval.',
+        });
+      }
 
-            const cloudinaryResult = await cloudinary.uploader.upload(req.file.path);
+      if (!req.file) {
+        return res
+          .status(400)
+          .json({ success: false, message: 'No file provided' });
+      }
 
-            if (!req.file) {
-                return res.status(400).json({ success: false, message: 'No file provided' });
-            }
+      const cloudinaryResult = await cloudinary.uploader.upload(req.file.path);
 
-            const requiredFields = ['companyDescription', 'jobTitle', 'country', 'rateFrom', 'rateTo', 'viewsRequired', 'jobDescription'];
-            const missingFields = requiredFields.filter(field => !req.body[field]);
+      const requiredFields = [
+        'companyDescription',
+        'jobTitle',
+        'country',
+        'rateFrom',
+        'rateTo',
+        'viewsRequired',
+        'jobDescription',
+      ];
+      const missingFields = requiredFields.filter((field) => !req.body[field]);
 
-            if (missingFields.length > 0) {
-                return res.status(400).json({ success: false, message: `Missing required fields: ${missingFields.join(', ')}` });
-            }
+      if (missingFields.length > 0) {
+        return res.status(400).json({
+          success: false,
+          message: `Missing required fields: ${missingFields.join(', ')}`,
+        });
+      }
 
-            const newImage = new CampaignUpload({
-                company: company._id,
-                companyDescription: req.body.companyDescription,
-                imageUrl: cloudinaryResult.secure_url,
-                jobTitle: req.body.jobTitle,
-                country: req.body.country,
-                rateFrom: req.body.rateFrom,
-                rateTo: req.body.rateTo,
-                viewsRequired: req.body.viewsRequired,
-                jobDescription: req.body.jobDescription
-            });
+      const newImage = new CampaignUpload({
+        company: company._id,
+        companyDescription: req.body.companyDescription,
+        imageUrl: cloudinaryResult.secure_url,
+        jobTitle: req.body.jobTitle,
+        country: req.body.country,
+        rateFrom: req.body.rateFrom,
+        rateTo: req.body.rateTo,
+        viewsRequired: req.body.viewsRequired,
+        jobDescription: req.body.jobDescription,
+      });
 
-            const savedImage = await newImage.save();
+      const savedImage = await newImage.save();
 
-            res.status(200).json({
-                success: true,
-                message: 'File Uploaded!',
-                campaignUpload: {
-                    ...savedImage.toObject(),
-                    imageUrl: cloudinaryResult.secure_url,  
+      res.status(200).json({
+        success: true,
+        message: 'File Uploaded!',
+        campaignUpload: {
+          ...savedImage.toObject(),
+          imageUrl: cloudinaryResult.secure_url,
+        },
+      });
+    } catch (error) {
+      console.error({ error });
+      return res.status(500).json({
+        success: false,
+        message: 'Error uploading or saving the file',
+      });
+    }
+  },
+
+  getCampaignImageAndDesc: async (req, res) => {
+    const logoWithDescId = req.params.id;
+
+    try {
+      const logoWithDesc = await CampaignUpload.findById(
+        { _id: logoWithDescId },
+        { company: 0, __v: 0 }
+      );
+
+      res.status(200).json(logoWithDesc);
+    } catch (error) {
+      res.status(500).json({ status: false, message: error.message });
+    }
+  },
+  getAllCampaignImageAndDesc: async (req, res) => {
+    try {
+      const logoWithDesc = await CampaignUpload.find({}, { __v: 0 });
+      res.status(200).json(logoWithDesc);
+    } catch (error) {
+      res.status(500).json({ status: false, message: error.message });
+    }
+  },
+  searchCampaign: async (req, res) => {
+    try {
+      const results = await CampaignUpload.aggregate([
+        [
+          {
+            $search: {
+              index: 'campaignsearch',
+              text: {
+                query: req.params.key,
+                path: {
+                  wildcard: '*',
                 },
-            });
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ success: false, message: 'Error uploading or saving the file' });
-        }
-    },
-
-    getCampaignImageAndDesc: async (req, res) => {
-        const logoWithDescId = req.params.id;
-    
-        try {
-            const logoWithDesc = await CampaignUpload.findById({ _id: logoWithDescId}, {company: 0, __v: 0});
-
-            res.status(200).json(logoWithDesc);
-    
-        } catch (error) {
-            res.status(500).json({ status: false, message: error.message });
-        }
-    },
-    getAllCampaignImageAndDesc: async (req, res) => {
-                try {
-                    const logoWithDesc = await CampaignUpload.find({}, { __v: 0 })
-                    res.status(200).json(logoWithDesc);
-                } catch (error) {
-                    res.status(500).json({ status: false, message: error.message });
-                }
+              },
             },
-            searchCampaign: async (req, res)=> {
-                try {
-                    const results = await CampaignUpload.aggregate([
-                        [
-                            {
-                              $search: {
-                                index: "campaignsearch",
-                                text: {
-                                  query: req.params.key,
-                                  path: {
-                                    wildcard: "*"
-                                  }
-                                }
-                              }
-                            }
-                          ]
-                      ])
-                      res.status(200).json(results)
-                } catch (error) {
-                   res.status(500).json(error); 
-                }
-            },
+          },
+        ],
+      ]);
+      res.status(200).json(results);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 };
--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-const express = require('express')
+const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const bodyParser = require("body-parser")
-const helmet = require('helmet')
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 // const rateLimit = require('express-rate-limit')
-const sanitize = require('express-mongo-sanitize')
-const app = express()
-const port = 6002
+const sanitize = require('express-mongo-sanitize');
+const multer = require('multer');
+const app = express();
+const port = 6002;
 
-const authRouter = require('./routes/auth')
-const userRouter = require('./routes/user')
-const verifyCompanyRouter = require('./routes/verifyCompany')
-const uploadRouter = require('./routes/campaingnUpload')
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
+const verifyCompanyRouter = require('./routes/verifyCompany');
+const uploadRouter = require('./routes/campaingnUpload');
 const influencerProfileRouter = require('./routes/influencerProfile');
-const nicheRouter = require('./routes/niche')
-const previewCampaignRouter = require('./routes/previewCampagn')
-const publishCampaignRouter = require('./routes/publishCampaign')
-const influencerApplicationRouter = require('./routes/influencerApplication')
+const nicheRouter = require('./routes/niche');
+const previewCampaignRouter = require('./routes/previewCampagn');
+const publishCampaignRouter = require('./routes/publishCampaign');
+const influencerApplicationRouter = require('./routes/influencerApplication');
 
 dotenv.config();
 
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
+  credential: admin.credential.cert(serviceAccount),
+});
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(helmet());
 app.use(sanitize());
 
@@ -45,13 +47,15 @@ app.use(sanitize());
 // app.use('/api', limiter)
 
 app.use('/api/', authRouter);
-app.use('/api/users', userRouter )
-app.use('/api/verifyCompany', verifyCompanyRouter )
-app.use('/api/uploadCampaign', uploadRouter)
-app.use('/api/influencerProfile',influencerProfileRouter )
+app.use('/api/users', userRouter);
+app.use('/api/verifyCompany', verifyCompanyRouter);
+app.use('/api/uploadCampaign', uploadRouter);
+app.use('/api/influencerProfile', influencerProfileRouter);
 app.use('/api/niches', nicheRouter);
 app.use('/api/preview', previewCampaignRouter);
 app.use('/api/publish', publishCampaignRouter);
-app.use('/api/influencer', influencerApplicationRouter)
+app.use('/api/influencer', influencerApplicationRouter);
 
-app.listen(process.env.PORT || port, () => console.log(`Flymedia is listening to port ${process.env.PORT}!`))
+app.listen(process.env.PORT || port, () =>
+  console.log(`Flymedia is listening to port ${process.env.PORT}!`)
+);
