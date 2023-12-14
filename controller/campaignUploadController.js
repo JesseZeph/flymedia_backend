68224d7a0e68922b03884d71288835f2f5d5923c
Shalom Abrokwah <shalomabrokwah@gmail.com>
2023-12-14 11:58:06 +0100

    Bug fixed

--- controller/campaignUploadController.js
+++ controller/campaignUploadController.js
@@ -68,8 +68,7 @@ module.exports = {
       console.error({ error });
       return res.status(500).json({
         success: false,
-        message: error.message,
-        // message: 'Error uploading or saving the file',
+        message: 'Error uploading or saving the file',
       });
     }
   },
