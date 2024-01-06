af58cf12ff389c69198515407f7cc83b9c3ccdf5
Jesse Josiah <jessezephyr@gmail.com>
2024-01-06 01:20:38 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const authMiddleware = require('../middleware/verifyToken')
 
 router.post('/register', authController.createUser);
 router.post('/register/influencer', authController.createInfluencer);
-router.post('/register/admin', authMiddleware.verifyAdmin, authController.createAdmin);
+router.post('/register/admin', authMiddleware.verifySuperAdmin, authController.createAdmin);
 router.post('/register/superAdmin', authController.createSuperAdmin);
 router.post('/login', authController.loginUser);
 router.post('/forgotPassword', authController.forgotPassword);
