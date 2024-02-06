14e9d4eb1e6ff963acf94a32b716c0bce91474f0
Jesse Josiah <jessezephyr@gmail.com>
2024-02-06 20:37:14 +0100

    stripe webhook

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const sanitize = require('express-mongo-sanitize');
+const Stripe = require('stripe');
+
 const app = express();
 const port = 6002;
 
@@ -21,8 +23,6 @@ const appleRouter = require('./routes/appleRouter');
 const subscriptionRouter = require('./routes/subscriptionRoutes');
 const accountRouter = require('./routes/accountRouter');
 const paymentRouter = require('./routes/paymentRouter');
-const stripeWebhookHandler = require('./middleware/stripeWebhook'); 
-
 
 dotenv.config();
 
@@ -38,16 +38,25 @@ mongoose
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-URL_CONFIG="https://flymediabackend-production.up.railway.app"
 
-app.use(URL_CONFIG + '/api/webhooks', express.raw({ type: 'application/json' }))
+
+webhookSecret = "whsec_AeqnnLKiS8h2BfCikqxfcplSBqjP2Gwl";
+
+app.use((req, res, next) => {
+  if (req.originalUrl === '/api/webhooks') {
+    next();
+  } else {
+    express.json()(req, res, next);
+  }
+});
+
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(helmet());
 app.use(sanitize());
-
 
 app.use('/api/', authRouter);
 app.use('/api/users', userRouter);
@@ -64,10 +73,40 @@ app.use('/api/account', accountRouter);
 app.use('/apple', appleRouter);
 app.use('/api/checkout', paymentRouter);
 
-app.post('/api/webhooks', stripeWebhookHandler.stripeListenWebhook);
+app.post(
+  '/api/webhooks',
+  express.raw({ type: 'application/json' }),
+  (req, res) => {
+    const sig = req.headers['stripe-signature'];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    } catch (err) {
+      console.log(`❌ Error message: ${err.message}`);
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    console.log('✅ Success:', event.id);
+
+    if (event.type === 'invoice.payment_succeeded') {
+      const stripeObject = event.data.object;
+      console.log(`💰 Invoice payment succeeded: ${stripeObject.status}`);
+    } else if (event.type === 'charge.succeeded') {
+      const charge = event.data.object;
+      console.log(`💵 Charge id: ${charge.id}`);
+    } else {
+      console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+    }
+
+    res.json({ received: true });
+  }
+);
 
 
 
 app.listen(process.env.PORT || port, () =>
-  console.log(`Flymedia is listening to port ${process.env.PORT}!`)
+  console.log(`Flymedia is listening to port ${process.env.PORT || port}!`)
 );
