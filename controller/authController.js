1d5c96970ccd46ac7c5159f77843dee63c68cec8
Jesse Josiah <jessezephyr@gmail.com>
2024-02-26 11:45:40 +0100

    update

--- controller/authController.js
+++ controller/authController.js
@@ -214,6 +214,7 @@ module.exports = {
       res.status(200).json({
         ...others,
         userToken,
+        userType,
         company: userCompany ? userCompany : {},
       });
     } catch (error) {
