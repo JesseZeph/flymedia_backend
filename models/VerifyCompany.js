ddde7f153c3ccc4c41a1a436898c79ab8b11f9c6
Jesse Josiah <jessezephyr@gmail.com>
2024-04-01 11:56:28 +0100

    update

--- models/VerifyCompany.js
+++ models/VerifyCompany.js
@@ -4,7 +4,7 @@ const CompanySchema = new mongoose.Schema(
   {
     companyName: { type: String, required: true },
     companyHq: { type: String, required: true },
-    website: { type: String, required: true },
+    website: { type: String, required: false },
     companyEmail: { type: String, required: true },
     memberContact: { type: String, required: true },
     isVerified: { type: Boolean, default: false },
