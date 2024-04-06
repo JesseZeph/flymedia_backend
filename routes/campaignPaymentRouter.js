2a45fe1199318bfcc2b2d0d1806ed63572329807
Shalom <shalomabrokwah@gmail.com>
2024-04-06 14:41:32 +0100

    Minor fix

--- routes/campaignPaymentRouter.js
+++ routes/campaignPaymentRouter.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const CampaignPaymentController = require('../controller/campaignPaymentController');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 router.post(
   '/initiate-payment',
   CampaignPaymentController.initiateCampaignPayment
 );
 router.post('/payment-success', CampaignPaymentController.paymentSuccess);
-router.post('/webhook', CampaignPaymentController.webhookHandler);
+router.post(
+  '/webhook',
+  bodyParser.raw({ type: 'application/json' }),
+  CampaignPaymentController.webhookHandler
+);
 
 module.exports = router;
